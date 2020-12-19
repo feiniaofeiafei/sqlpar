7d7d760c79da990c4505a6015ef194e80a76946c
feiniaofeiafei <mal584040934@163.com>
2020-12-19 11:06:06 +0800

    add Expr.cpp and Expr.h

--- sql/Expr.h
+++ sql/Expr.h
@@ -81,7 +81,80 @@ namespace hsql{
         kDatetimeMonth,
         kDatetimeYear,
     };
+    typedef struct Expr Expr;
 
+    //Represents SQL expressions(i.e. literals,operators,column_refs)
+    //TODO:When destructing a placeholder expression,we minght need to alter the placeholder_list
+    struct Expr{
+        Expr(ExprType type);
+        virtual ~Expr();
+
+        ExprType type;
+
+        //TODO: Replace expressions by list.
+        Expr* expr;
+        Expr* expr2;
+        std::vector<Expr*>* exprList;
+        SelectStatement* select;
+        char* name;
+        char* table;
+        char* alias;
+        double fval;
+        int64_t ival;
+        int64_t ival2;
+        DatetimeField datetimeField;
+        ColumnType columnType;
+        bool isBoolLiteral;
+        OperatorType opType;
+        bool distinct;
+
+        //Convenience accessor methods.
+
+        bool isType(ExprType exprType) const;
+        bool isLiteral() const;
+        bool hasAlias() const;
+        bool hasTable() const;
+        const char* getName() const;
+
+        //Static constructors.
+        static Expr* make(ExprType type);
+        static Expr* makeOpUnary(OperatorType op,Expr* expr);
+        static Expr* makeBetween(Expr* expr,Expr* left,Expr* right);
+        static Expr* makeCaseList(Expr* caseListElement);
+        static Expr* makeCaseListElement(Expr* when,Expr* then);
+        static Expr* caseListAppend(Expr* caseList,Expr* caseListElement);
+        static Expr* makeCase(Expr* expr,Expr* when,Expr* elseExpr);
+        static Expr* makeLiteral(int64_t val);
+        static Expr* makeLiteral(double val);
+        static Expr* makeLiteral(char* val);
+        static Expr* makeLiteral(bool val);
+        static Expr* makeNullLiteral();
+        static Expr* makeColumnRef(char* name);
+        static Expr* makeStar(void);
+        static Expr* makeStar(char* table);
+        static Expr* makeFunctionRef(char* func_name,std::vector<Expr*>* exprList,bool distinct);
+        static Expr* makeArray(std::vector<Expr*>* exprList);
+        static Expr* makeArrayIndex(Expr* expr,int64_t index);
+        static Expr* makeParameter(int id);
+        static Expr* makeSelect(SelectStatement* select);
+        static Expr* makeExists(SelectStatement* select);
+        static Expr* makeInOperator(Expr* expr,std::vector<Expr*>*exprList);
+        static Expr* makeInOperator(Expr* expr,SelectStatement* select);
+        static Expr* makeExtract(DatetimeField datetimeField1,Expr* expr);
+        static Expr* makeCast(Expr* expr,ColumnType columnType);
+    };
+    //Zero initializes an Expr object and assigns it to a space in the heap.
+    //For Hyrise we still had to put in the explicit NULL constructor
+    
+    //unused
+    #define ALLOC_EXPR(var,type)
+    Expr* var;
+    do{
+        Expr zero={type};
+        var = (Expr*)malloc(sizeof *var);
+        *var=zero;
+    }while(0);
+    #undef ALLOC_EXPR
 }
 
 #endif
